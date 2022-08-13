1365841afc7d233a1272176aaa812ab4559c19ca
qmost <qmostafa200@gmail.com>
2022-08-13 20:06:39 +0200

    we add the list items fuile  and then add the list conent { the list content have a slidaple action } Note** the function of the button stell not work

--- lib/theme_data.dart
+++ lib/theme_data.dart
@@ -7,15 +7,25 @@ class MyThemeData {
   static const Color red = Color(0xFFEC4B4B);
 
   static final lightTheme = ThemeData(
-      primaryColor: primarycolor,
-      scaffoldBackgroundColor: scaffoldBacgroundcolor,
-      bottomNavigationBarTheme: BottomNavigationBarThemeData(
-          backgroundColor: Colors.transparent,
-          elevation: 0,
-          unselectedIconTheme: IconThemeData(
-            size: 32,
-          ),
-          selectedIconTheme: IconThemeData(
-            size: 32,
-          )));
+    primaryColor: primarycolor,
+    scaffoldBackgroundColor: scaffoldBacgroundcolor,
+    bottomNavigationBarTheme: BottomNavigationBarThemeData(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        unselectedIconTheme: IconThemeData(
+          size: 32,
+        ),
+        selectedIconTheme: IconThemeData(
+          size: 32,
+        )),
+    textTheme: TextTheme(
+        titleMedium: TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+          color: primarycolor,
+        ),
+        bodyMedium: TextStyle(
+          fontSize: 16,
+        )),
+  );
 }
